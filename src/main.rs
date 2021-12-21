474888848f678e5db9c21121eb312b80484b9b4c
Liz Ainslie <lizainslie16@gmail.com>
2021-12-21 06:17:10 -0600

    Make it echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! Type some text.");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read input");
+
+    println!("You said: {}", guess);
 }
