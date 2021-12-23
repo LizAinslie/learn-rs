c98d938aa9bd5a9162a99c6b3ebf7ffe86ab64b2
Liz Ainslie <lizainslie16@gmail.com>
2021-12-23 16:32:28 -0600

    Make the guessing game example from the book.
    I added my own code to make it extra user-friendly and cool :)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Hello, world! Type some text.");
+    println!("Welcome to the number guessing game.");
+    let secret_num = rand::thread_rng().gen_range(1..101);
+    println!("Shh! Secret number is {}", secret_num);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read input");
 
-    println!("You said: {}", guess);
+    loop {
+        eprint!("Guess a number: ");
+
+        let mut guess_str = String::new();
+        io::stdin().read_line(&mut guess_str).expect("Failed to read input.");
+        let guess_result = guess_str.trim().parse();
+
+        if let Err(_) = guess_result {
+            println!("Input must be a number.");
+            continue;
+        }
+
+        let guess_num: u32 = guess_result.unwrap();
+        println!("You guessed {}.", guess_num);
+        match guess_num.cmp(&secret_num) {
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Less => println!("Too low!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
